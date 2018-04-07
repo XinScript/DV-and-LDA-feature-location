00269460a42daf309a2844447a1cce8969e2cddd
XinScript <hobsonchan1992@gmail.com>
2018-04-07 15:38:41 +0100

    ls

---
+++ src/Project.py
@@ -0,0 +1,8 @@
+# import collections
+# class Project(object):
+#     def __init__(self,data_path):
+#         self.data_path = data_path
+#         self.project_name,t = [for x in x data_path.split('/') if x][-1].split('_')
+#         collections.nam
+        
+
