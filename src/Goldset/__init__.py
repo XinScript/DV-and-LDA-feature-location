8349bc18344442619d890ffc3bddb7c74c08b258
XinScript <hobsonchan1992@gmail.com>
2018-04-19 21:16:37 +0100

    bug fix

--- src/Goldset/__init__.py
+++
@@ -1,6 +0,0 @@
-from .generator import GoldsetGenerator
-from .bycommit import CommitGoldsetGenerator
-from .byissue import IssueGoldsetGenerator
-
-__all__ = ['GoldsetGenerator','IssueGoldsetGenerator','CommitGoldsetGenerator']
-
